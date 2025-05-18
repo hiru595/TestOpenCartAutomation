0a194b0615af621feed44838c1f1af32f27c3fc4
hiru595 <hirenthakkar595@gmail.com>
2025-05-18 22:37:24 +0530

    Chromedriver added instead of other browser.

--- src/test/java/baseTest/CommonToAllTests.java
+++ src/test/java/baseTest/CommonToAllTests.java
@@ -13,7 +13,7 @@ public class CommonToAllTests {
     public WebDriver driver;
     @BeforeMethod
     public void StartBrowser(){
-        driver=new EdgeDriver();
+        driver=new ChromeDriver();
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
